850c8c0ffd797d9e4c850489489da95b2be0cac6
OmniDev951 <ochsjax000@alpinesd.org>
2025-11-05 12:26:48 -0700

    Added editor functionality

--- Assets/Menu_Script.cs
+++ Assets/Menu_Script.cs
@@ -1,15 +1,27 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-//Nessecary Components
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+// Nessecary Components
 public class Menu_Script : MonoBehaviour
 {
-      public void play()
-      {
-              SceneManager.LoadScene("Game_Scene");
-      }
-      public void quit()
-      {
-              Application.Quit();
-      }
+    public void play()
+    {
+        SceneManager.LoadScene("Game_Scene");
+    }
+
+    // Public parameterless method so it can be wired to a UI Button OnClick
+    public void quit()
+    {
+#if UNITY_EDITOR
+        // In the Editor Application.Quit does nothing. Stop play mode so the button is testable during development.
+        EditorApplication.isPlaying = false;
+#else
+        // In a build this will close the application.
+        Application.Quit();
+#endif
+    }
 }
